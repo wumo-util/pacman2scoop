605d598de8621db475e6e01420973df35840c4f8
WuMo <wumo@outlook.com>
2020-06-06 16:52:00 +0800

    [release] 0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.github.wumo"
-version = "0.0.2"
+version = "0.0.1"
 
 repositories {
   mavenCentral()
--- src/main/kotlin/com/github/wumo/Main.kt
+++ src/main/kotlin/com/github/wumo/Main.kt
@@ -1,6 +1,5 @@
 package com.github.wumo
 
-import com.github.wumo.console.ArgParser
 import java.io.File
 
 fun main(args: Array<String>) {
@@ -9,13 +8,12 @@ fun main(args: Array<String>) {
     val requiredPackages = Online.dependenciesOf(args.toList())
     
     val content = buildString {
-      requiredPackages.forEach {
-        appendln("\"${it.url}\"")
-      }
-      appendln()
-      requiredPackages.forEach {
-        appendln("\"${it.sha256}\"")
-      }
+      appendln("\"url\": [")
+      appendln(requiredPackages.joinToString(",\n") { "\"${it.url}\"" })
+      appendln("],")
+      appendln("\"hash\": [")
+      appendln(requiredPackages.joinToString(",\n") { "\"${it.sha256}\"" })
+      appendln("]")
     }
     File("packages.txt").writeText(content)
     println("results have been written to packages.txt.")
